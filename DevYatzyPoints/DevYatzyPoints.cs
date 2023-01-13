1ee9771ce28bca3ef76733f66635894bc92167ee
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2023-01-13 17:36:40 +0100

    separate method pointsForOnesToSixes

--- DevYatzyPoints/DevYatzyPoints.cs
+++ DevYatzyPoints/DevYatzyPoints.cs
@@ -17,13 +17,42 @@ public class DevYatzyPoints
         fours,
         fives,
         sixes,
+
+        pair,
+        two_pair,
+        three_of_a_kind,
+        four_of_a_kind,
+        small_straight,
+        big_straight,
+        full_house,
+        chance,
+        yatzy
     }
 
     public static int points(string eyes, string category)
     {
         Enum.TryParse(category.ToLower(), out Category enumCategory);
 
-        int categoryValue = (int) enumCategory;
+        switch(enumCategory)
+        {
+            case Category.pair:
+                return 0;
+
+            case Category.ones:
+            case Category.twos:
+            case Category.threes:
+            case Category.fours:
+            case Category.fives:
+            case Category.sixes:
+                return pointsForOnesToSixes(eyes, enumCategory);
+        }
+
+        return pointsForOnesToSixes(eyes, enumCategory);
+    }
+
+    private static int pointsForOnesToSixes(string eyes, Category enumCategory)
+    {
+        int categoryValue = (int)enumCategory;
         char search = char.Parse(categoryValue.ToString());
 
         int occuranceAmount = eyes.Count(eye => eye == search);
