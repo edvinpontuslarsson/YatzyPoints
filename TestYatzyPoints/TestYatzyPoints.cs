41c8c11409b11d6419a0beef1493b7438ff47cd8
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2023-01-13 19:23:45 +0100

    TestPair()

--- TestYatzyPoints/TestYatzyPoints.cs
+++ TestYatzyPoints/TestYatzyPoints.cs
@@ -22,13 +22,29 @@ public class TestYatzyPoints
         int three = DevYatzyPoints.points("3, 3, 3, 1, 1", "threes");
         int four = DevYatzyPoints.points("4, 4, 4, 4, 1", "Fours");
         int five = DevYatzyPoints.points("5, 5, 5, 5, 5", "fives");
+        
         int aSix = DevYatzyPoints.points("1, 1, 1, 1, 6", "Sixes");
+        int none = DevYatzyPoints.points("1, 1, 1, 1, 2", "Sixes");
 
         Assert.AreEqual(one, 1 * 1);
         Assert.AreEqual(two, 2 * 2);
         Assert.AreEqual(three, 3 * 3);
         Assert.AreEqual(four, 4 * 4);
         Assert.AreEqual(five, 5 * 5);
+        
         Assert.AreEqual(aSix, 1 * 6);
+        Assert.AreEqual(none, 0);
+    }
+
+    [TestMethod]
+    public void TestPair()
+    {
+        int resultA = DevYatzyPoints.points("2,2,3,3,3", "pair");
+        int resultB = DevYatzyPoints.points("3,4,4,4,6", "pair");
+        int resultC = DevYatzyPoints.points("0,0,0,0,0", "pair");
+
+        Assert.AreEqual(resultA, 6);
+        Assert.AreEqual(resultB, 8);
+        Assert.AreEqual(resultC, 0);
     }
 }
