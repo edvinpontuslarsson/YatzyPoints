28256d1537b1b54e19c1fc56ea5c7d3b5f0226b9
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2023-01-13 17:07:27 +0100

    removes comments

--- DevYatzyPoints/DevYatzyPoints.cs
+++ DevYatzyPoints/DevYatzyPoints.cs
@@ -9,17 +9,6 @@ namespace DevYatzyPoints;
 
 public class DevYatzyPoints
 {
-    // TODO OK, start with just counting ones
-
-    // instruction is string with eyes
-    // category can be enum I think but start with string
-    // ("1, 2,3,1,1", "ones") 3 ones => 3 * 1 == 3
-
-    // "5, 2, 4, 4, 1" for kategorien firere gir 8 poeng (2 firere = 2 x 4 = 8)
-    // ("5, 2, 4, 4, 1", "fours") 2 fours => 2 * 4 == 8
-
-    // TODO remove whitespaces, split by ","
-
     enum Category
     {
         ones = 1,
