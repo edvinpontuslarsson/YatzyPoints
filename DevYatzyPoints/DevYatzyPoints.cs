beb66be59048d7932af7d7d1720b0ea9cce8450b
Edvin Larsson <edvinpontuslarsson@gmail.com>
2023-01-14 11:33:19 +0100

    Calculations and tests for more categories (#4)
    * separate method pointsForOnesToSixes
    
    * method Duplicates
    
    * pair code written but not tested
    
    * TestPair()
    
    * twopair works, bug in pair
    
    * fixes duplicates.Count == 0 bug
    
    * nicer eyes
    
    * unused FrequencyTable method
    
    * GREEN ThreeOfAKind
    
    * GREEN XOfAKind
    
    * GREEN Straight
    
    * todo comments

--- DevYatzyPoints/DevYatzyPoints.cs
+++ DevYatzyPoints/DevYatzyPoints.cs
@@ -17,17 +17,189 @@ public class DevYatzyPoints
         fours,
         fives,
         sixes,
+
+        pair,
+        two_pair,
+        three_of_a_kind,
+        four_of_a_kind,
+        small_straight,
+        big_straight,
+        full_house,
+        chance,
+        yatzy
+    }
+
+    enum XOfAKind
+    {
+        three = 3,
+        four = 4,
+    }
+
+    enum Straight
+    {
+        small = 15,
+        big = 20,
     }
 
     public static int points(string eyes, string category)
     {
         Enum.TryParse(category.ToLower(), out Category enumCategory);
 
-        int categoryValue = (int) enumCategory;
+        // removes any whitespaces
+        eyes = Regex.Replace(eyes, @"\s+", "");
+
+        // TODO validate input
+
+        switch (enumCategory)
+        {
+            case Category.ones:
+            case Category.twos:
+            case Category.threes:
+            case Category.fours:
+            case Category.fives:
+            case Category.sixes:
+                return pointsForOnesToSixes(eyes, enumCategory);
+
+            case Category.pair:
+                return pointsForPair(eyes);
+
+            case Category.two_pair:
+                return pointsForTwoPair(eyes);
+
+            case Category.three_of_a_kind:
+                return pointsForXOfAKind(eyes, XOfAKind.three);
+
+            case Category.four_of_a_kind:
+                return pointsForXOfAKind(eyes, XOfAKind.four);
+
+            case Category.small_straight:
+                return pointsForStraight(eyes, Straight.small);
+
+            case Category.big_straight:
+                return pointsForStraight(eyes, Straight.big);
+
+            // TODO fulll house, use frequencyTable again
+            // TODO sjanse/chance is very similar to my straight, but simpler
+            // TODO Yatzy just make sure all the same then 50 else 0
+        }
+
+        return pointsForOnesToSixes(eyes, enumCategory);
+    }
+
+    private static int pointsForOnesToSixes(string eyes, Category enumCategory)
+    {
+        int categoryValue = (int)enumCategory;
         char search = char.Parse(categoryValue.ToString());
 
-        int occuranceAmount = eyes.Count(eye => eye == search);
+        int occuranceAmount = eyes.Count(character => character == search);
 
         return occuranceAmount * categoryValue;
+    }
+
+    private static int pointsForPair(string eyes)
+    {
+        List<int> duplicates = Duplicates(eyes);
+
+        if (duplicates.Count == 0) return 0;
+
+        int maxValue = duplicates.Max();
+
+        return maxValue * 2;
+    }
+
+    private static int pointsForTwoPair(string eyes)
+    {
+        List<int> duplicates = Duplicates(eyes);
+
+        int sumOfPairs = 0;
+
+        foreach (int pair in duplicates) 
+        {
+            sumOfPairs += pair * 2;
+        }
+
+        return sumOfPairs;
+    }
+
+    private static int pointsForXOfAKind(string eyes, XOfAKind xOfAKind)
+    {
+        // x is 3 or 4
+        int x = (int)xOfAKind;
+
+        Dictionary<int, int> frequencyTable = FrequencyTable(eyes);
+        
+        // if any occurs x or more times
+        // return that multiplied with x
+        foreach (KeyValuePair<int, int> keyValue in frequencyTable)
+        {
+            if (keyValue.Value >= x) return keyValue.Key * x;
+        }
+
+        return 0;
+    }
+
+    private static int pointsForStraight(string eyes, Straight straight)
+    {
+        int sum = 0;
+
+        string[] justEyes = eyes.Split(',');
+
+        foreach (string eye in justEyes)
+        {
+            sum += int.Parse(eye);
+        }
+
+        // straight is 15 for small, 20 for big
+        if (sum != (int)straight) return 0;
+
+        return sum;
+    }
+
+    private static List<int> Duplicates(string eyes)
+    {
+        List<int> duplicates = new List<int>();
+        
+        string[] justEyes = eyes.Split(',');
+
+        foreach (string character in justEyes) 
+        {
+            int value = int.Parse(character);
+
+            if (duplicates.Contains(value)) continue;
+
+            int indexOfFirstOccurance = eyes.IndexOf(character);
+            int indexOfLastOccurance = eyes.LastIndexOf(character);
+
+            // if first and last index isn't the same, it occurs more than once
+            if (indexOfFirstOccurance != indexOfLastOccurance) 
+            {
+                duplicates.Add(value);
+            }
+        }
+
+        return duplicates;
+    }
+    
+    private static Dictionary<int, int> FrequencyTable(string eyes)
+    {
+        string[] justEyes = eyes.Split(',');
+
+        Dictionary<int, int> frequencyTable = new Dictionary<int, int>();
+
+        foreach (string eye in justEyes)
+        {
+            int numericEye = int.Parse(eye);
+
+            if (frequencyTable.ContainsKey(numericEye))
+            {
+                frequencyTable[numericEye] += 1;
+            }
+            else
+            {
+                frequencyTable[numericEye] = 1;
+            }
+        }
+
+        return frequencyTable;
     }
 }
--- TestYatzyPoints/TestYatzyPoints.cs
+++ TestYatzyPoints/TestYatzyPoints.cs
@@ -22,13 +22,87 @@ public class TestYatzyPoints
         int three = DevYatzyPoints.points("3, 3, 3, 1, 1", "threes");
         int four = DevYatzyPoints.points("4, 4, 4, 4, 1", "Fours");
         int five = DevYatzyPoints.points("5, 5, 5, 5, 5", "fives");
+        
         int aSix = DevYatzyPoints.points("1, 1, 1, 1, 6", "Sixes");
+        int none = DevYatzyPoints.points("1, 1, 1, 1, 2", "Sixes");
 
         Assert.AreEqual(one, 1 * 1);
         Assert.AreEqual(two, 2 * 2);
         Assert.AreEqual(three, 3 * 3);
         Assert.AreEqual(four, 4 * 4);
         Assert.AreEqual(five, 5 * 5);
+        
         Assert.AreEqual(aSix, 1 * 6);
+        Assert.AreEqual(none, 0);
+    }
+
+    [TestMethod]
+    public void TestPair()
+    {
+        int resultA = DevYatzyPoints.points("2,2,3,3,3", "pair");
+        int resultB = DevYatzyPoints.points("3,4,4,4,6", "pair");
+        int resultC = DevYatzyPoints.points("1,2,3,4,5", "pair");
+
+        Assert.AreEqual(resultA, 6);
+        Assert.AreEqual(resultB, 8);
+        Assert.AreEqual(resultC, 0);
+    }
+
+    [TestMethod]
+    public void TestTwoPair()
+    {
+        int resultA = DevYatzyPoints.points("3, 3, 1, 5, 5", "two_pair"); // 16
+        int resultB = DevYatzyPoints.points("4, 5, 1, 5, 3", "two_pair");
+        int resultC = DevYatzyPoints.points("1,2,3,4,5", "two_pair");
+
+        Assert.AreEqual(resultA, 16);
+        Assert.AreEqual(resultB, 10);
+        Assert.AreEqual(resultC, 0);
+    }
+
+    [TestMethod]
+    public void TestThreeOfAKind()
+    {
+        int resultA = DevYatzyPoints.points("5, 1, 5, 2, 5", "three_of_a_kind"); // 15
+        int resultB = DevYatzyPoints.points("5, 5, 5, 2, 5", "three_of_a_kind"); // still 15
+        int resultC = DevYatzyPoints.points("5,5,2,2,1", "three_of_a_kind");
+
+        Assert.AreEqual(resultA, 15);
+        Assert.AreEqual(resultB, 15);
+        Assert.AreEqual(resultC, 0);
+    }
+
+    [TestMethod]
+    public void TestFourOfAKind()
+    {
+        int resultA = DevYatzyPoints.points("5,5, 2, 5,5", "four_of_a_kind"); // 20
+        int resultB = DevYatzyPoints.points("5, 1, 5, 2, 5", "four_of_a_kind"); // 0
+        int resultC = DevYatzyPoints.points("5,5,2,2,1", "four_of_a_kind");
+
+        Assert.AreEqual(resultA, 20);
+        Assert.AreEqual(resultB, 0);
+        Assert.AreEqual(resultC, 0);
+    }
+
+    [TestMethod]
+    public void TestStraight()
+    {
+        int smallStraight = DevYatzyPoints.points("1,2,3,4,5", "small_straight");
+        int bigStraight = DevYatzyPoints.points("2,3,4,5,6", "big_straight");
+
+        int wantsBigStraightButIsSmall = DevYatzyPoints.points("1,2,3,4,5", "big_straight");
+        int wantsSmallStraightButIsBig = DevYatzyPoints.points("2,3,4,5,6", "small_straight");
+
+        int notStraight = DevYatzyPoints.points("1,2,3,4,6", "small_straight");
+        int notStraightBig = DevYatzyPoints.points("2,3,5,5,6", "big_straight");
+
+        Assert.AreEqual(smallStraight, 15);
+        Assert.AreEqual(bigStraight, 20);
+
+        Assert.AreEqual(wantsBigStraightButIsSmall, 0);
+        Assert.AreEqual(wantsSmallStraightButIsBig, 0);
+
+        Assert.AreEqual(notStraight, 0);
+        Assert.AreEqual(notStraightBig, 0);
     }
 }
